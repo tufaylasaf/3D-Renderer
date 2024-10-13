d46b88c7ec1653db76938d9a183f7b2e789d57d5
Tufayl <tufaylasaf@gmail.com>
2024-10-13 11:26:34 +0530

    Skybox and Cubemaps

--- main.cpp
+++ main.cpp
@@ -9,6 +9,39 @@ const unsigned int height = 900;
 
 const unsigned int samples = 8;
 
+float skyboxVertices[] =
+    {
+        //   Coordinates
+        -1.0f, -1.0f, 1.0f,  //        7--------6
+        1.0f, -1.0f, 1.0f,   //       /|       /|
+        1.0f, -1.0f, -1.0f,  //      4--------5 |
+        -1.0f, -1.0f, -1.0f, //      | |      | |
+        -1.0f, 1.0f, 1.0f,   //      | 3------|-2
+        1.0f, 1.0f, 1.0f,    //      |/       |/
+        1.0f, 1.0f, -1.0f,   //      0--------1
+        -1.0f, 1.0f, -1.0f};
+
+unsigned int skyboxIndices[] =
+    {
+        // Right
+        1, 2, 6,
+        6, 5, 1,
+        // Left
+        0, 4, 7,
+        7, 3, 0,
+        // Top
+        4, 5, 6,
+        6, 7, 4,
+        // Bottom
+        0, 3, 2,
+        2, 1, 0,
+        // Back
+        0, 1, 5,
+        5, 4, 0,
+        // Front
+        3, 7, 6,
+        6, 2, 3};
+
 int main()
 {
 
@@ -51,6 +84,10 @@ int main()
 
     Shader shaderProgram("res/shaders/default.vert", "res/shaders/default.frag");
     Shader lightShader("res/shaders/light.vert", "res/shaders/light.frag");
+    Shader skyboxShader("res/shaders/skybox.vert", "res/shaders/skybox.frag");
+
+    skyboxShader.Activate();
+    glUniform1i(glGetUniformLocation(skyboxShader.ID, "skybox"), 0);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -65,6 +102,73 @@ int main()
     Model model("res/models/monkey/monkey.gltf", "Monkey");
     Model model2("res/models/sphere/sphere.gltf", "Sphere");
     Model light("res/models/sphere/sphere.gltf", "Light");
+    Model cube("res/models/cube/cube.gltf", "Cube");
+    Model abstractBlock("res/models/block_shape_abstract/abstract.gltf", "AbstractBlock");
+
+    unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glGenBuffers(1, &skyboxEBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    // All the faces of the cubemap (make sure they are in this exact order)
+    std::string facesCubemap[6] =
+        {
+            "res/skybox/right.jpg",
+            "res/skybox/left.jpg",
+            "res/skybox/top.jpg",
+            "res/skybox/bottom.jpg",
+            "res/skybox/front.jpg",
+            "res/skybox/back.jpg"};
+
+    // Creates the cubemap texture object
+    unsigned int cubemapTexture;
+    glGenTextures(1, &cubemapTexture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    // These are very important to prevent seams
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+    // This might help with seams on some systems
+    // glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+    // Cycles through all the textures and attaches them to the cubemap object
+    for (unsigned int i = 0; i < 6; i++)
+    {
+        int width, height, nrChannels;
+        unsigned char *data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            stbi_set_flip_vertically_on_load(false);
+            glTexImage2D(
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0,
+                GL_RGB,
+                width,
+                height,
+                0,
+                GL_RGB,
+                GL_UNSIGNED_BYTE,
+                data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -100,7 +204,33 @@ int main()
 
         model.Draw(shaderProgram, camera);
         model2.Draw(shaderProgram, camera);
+        cube.Draw(shaderProgram, camera);
+        abstractBlock.Draw(shaderProgram, camera);
+
         light.Draw(lightShader, camera);
+
+        glDepthFunc(GL_LEQUAL);
+
+        skyboxShader.Activate();
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        // We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
+        // The last row and column affect the translation of the skybox (which we don't want to affect)
+        view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+        projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+        // Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
+        // where an object is present (a depth of 1.0f will always fail against any object's depth value)
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Switch back to the normal depth function
+        glDepthFunc(GL_LESS);
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -112,6 +242,9 @@ int main()
 
     model.SaveImGuiData("saveData/transforms.json");
     model2.SaveImGuiData("saveData/transforms.json");
+    cube.SaveImGuiData("saveData/transforms.json");
+    abstractBlock.SaveImGuiData("saveData/transforms.json");
+
     light.SaveImGuiData("saveData/transforms.json");
 
     ImGui_ImplOpenGL3_Shutdown();
